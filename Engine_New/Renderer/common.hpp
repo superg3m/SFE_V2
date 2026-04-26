5c39ecfe705270ef50c4c1fcd992379e63344f2c
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-04-25 21:12:04 -0400

    Update common.hpp

--- Engine_New/Renderer/common.hpp
+++ Engine_New/Renderer/common.hpp
@@ -2,6 +2,7 @@
 
 #include "../../Core/core.hpp"
 
+// NOTE(Jovanni): this is size in float
 enum class BufferStrideTypeInfo {
 	BOOL = 1,
 	INT = 1,
@@ -13,6 +14,15 @@ enum class BufferStrideTypeInfo {
 	MAT4 = 16
 };
 
+const float MAGIC_NUMBER = -123450510.0f;
+const int RESERVED_VERTEX_ATTRIBUTE_LOCATIONS = 3;
+
+struct Vertex {
+	Vec3 aPosition    = Vec3(MAGIC_NUMBER);   // location 0
+	Vec3 aNormal      = Vec3(MAGIC_NUMBER);   // location 1
+	Vec2 aTexCoord    = Vec2(MAGIC_NUMBER);   // location 2
+};
+
 struct VertexAttribute {
 	size_t offset = 0; // offset in bytes of the actual memeber! OFFSET_OF(Vertex, aPosition)
 	u32 location;
@@ -20,13 +30,40 @@ struct VertexAttribute {
 	bool instanced = false;
 };
 
+static u32 compute_stride_from_elements(Vector<VertexAttribute>& layout) {
+	u32 stride = 0;
+	for (VertexAttribute desc : layout) {
+		stride += (u32)desc.type * sizeof(float);
+	}
+
+	return stride;
+}
+
 struct VertexLayout {
 	u32 stride;
 	u32 stride_in_floats; // stride / sizeof(float)
 	Vector<VertexAttribute> attributes;
 
-	static VertexLayout& PNT();
-	static VertexLayout create(Vector<VertexAttribute> attributes);
+	/*
+	static VertexLayout& PNT() {
+        static VertexLayout layout = VertexLayout({
+            {OFFSET_OF(Vertex, aPosition), BufferStrideTypeInfo::VEC3},
+            {OFFSET_OF(Vertex, aNormal), BufferStrideTypeInfo::VEC3},
+            {OFFSET_OF(Vertex, aTexCoord), BufferStrideTypeInfo::VEC2},
+        });
+
+        return layout;
+    }
+	*/
+
+	VertexLayout(Vector<VertexAttribute>& attributes) : attributes(attributes) {
+        this->stride = 0;
+		for (VertexAttribute desc : attributes) {
+			this->stride += (u32)desc.type * sizeof(float);
+		}
+
+        this->stride_in_floats = this->stride / sizeof(float);
+    }
 };
 
 struct DepthState {
