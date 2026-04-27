601780c5ffa47d852afde5ba5801bae65aeae453
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-04-26 21:58:42 -0400

    cleaning up the old engine

--- Engine/OpenGL/opengl.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <mesh.hpp>
-#include <gpu_buffers.hpp>
-#include <material.hpp>
-#include <texture.hpp>
-#include <shader.hpp>
-#include <render_queue.hpp>
-#include <render_state.hpp>
