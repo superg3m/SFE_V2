e9eed02fe592a840526389709e552dba13395065
Jovanni D <80374076+superg3m@users.noreply.github.com>
2026-04-29 22:21:03 -0400

    Update game.cpp

--- Game/game.cpp
+++ Game/game.cpp
@@ -1,4 +1,4 @@
-#include <sfe_dll.hpp>
+#include <sfe.hpp>
 
 struct AppState {
 	ShaderHandle cube_shader = ShaderHandle::invalid();
@@ -148,11 +148,11 @@ extern "C" /*__declspec(dllexport)*/ void application_render(Engine* engine, flo
 
 	Pipeline pipeline = app->use_opaque_pipeline ? app->opaque_pipeline : app->opaque_wireframe_pipeline;
 	// engine->renderer.bind_vertex_buffer(app->cube_mesh, app->instance_cube_vbo);
-	engine->renderer.draw_mesh(pipeline, app->cube_mesh, model, view, projection, app->cube_translations.count);
+	// engine->renderer.draw_mesh(pipeline, app->cube_mesh, model, view, projection, app->cube_translations.count);
 
 	model = Mat4::translate(model, -5, 0, 0);
 	pipeline = !app->use_opaque_pipeline ? app->opaque_pipeline : app->opaque_wireframe_pipeline;
-	engine->renderer.draw_mesh(pipeline, app->backpack_mesh, model, view, projection);
+	// engine->renderer.draw_mesh(pipeline, app->backpack_mesh, model, view, projection);
 
 	/*
 	Mesh backpack_mesh = engine->renderer.backend.meshes.get(app.backpack_mesh_handle);
